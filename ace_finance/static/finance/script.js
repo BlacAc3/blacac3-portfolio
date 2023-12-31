dcbfa3b778843fa55b7779d2f989ef33f0abd98f
BlacAc3 <aceezeala@gmail.com>
2023-12-31 13:44:43 +0100

    Made UI changes

--- ace_finance/static/finance/script.js
+++ ace_finance/static/finance/script.js
@@ -6,9 +6,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
         sidebar.classList.toggle("hide")
         console.log("hidden")
     })
+
+    function showPopup(text) {
+        const popup = document.getElementById('topPopup');
+        const popupText =document.querySelector("#topPopup div")
+        popupText.innerHTML = text;
+        popup.style.display = 'flex';
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 3000); // Adjust the time the popup is displayed (in milliseconds)
+    }
     
-
-
-
+    let accountNumber = document.querySelector(".num");
+    accountNumber.addEventListener("click", ()=>{
+        let text= accountNumber.innerHTML
+        showPopup(`Copied! ${text}`)
+    })
 
 })
--- mainpage/static/mainpage/script.js
+++ mainpage/static/mainpage/script.js
@@ -82,5 +82,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
         event.stopPropagation();
     });
     
+
+
+    // copy function
+    function copyToClipboard(text) {
+        const textarea = document.createElement('textarea');
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+    }
     
 })
