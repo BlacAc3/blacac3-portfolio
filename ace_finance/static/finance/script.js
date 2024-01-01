b7d8cf1622256a9dcce43e87398be778bb2ecb72
BlacAc3 <aceezeala@gmail.com>
2024-01-01 20:51:41 +0100

    Added A transaction section

--- ace_finance/static/finance/script.js
+++ ace_finance/static/finance/script.js
@@ -17,10 +17,35 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }, 3000); // Adjust the time the popup is displayed (in milliseconds)
     }
     
+    // display popup for account number click event 
     let accountNumber = document.querySelector(".num");
-    accountNumber.addEventListener("click", ()=>{
-        let text= accountNumber.innerHTML
-        showPopup(`Copied! ${text}`)
+    if (accountNumber){
+        accountNumber.addEventListener("click", ()=>{
+                let text= accountNumber.innerHTML
+                showPopup(`Copied! ${text}`)
+            })
+    }
+    
+
+    //display popup when a user clicks receive
+    let receiveButton= document.querySelector(".receive");
+    receiveButton.addEventListener("click", ()=>{
+        console.log("clicked")
+        showPopup("Copy your account number By Clicking on it in your Profile Page")
+    })
+
+    //popup for transaction
+    let sendButton = document.querySelector(".send")
+    let modalContainer=document.querySelector(".modal-container-div")
+    sendButton.addEventListener("click", ()=>{
+        console.log("Send button Clicked")
+        modalContainer.style.display="flex";
+    })
+    //close transaction popup
+    let closeModal = document.querySelector(".modal-close-button")
+    closeModal.addEventListener("click", ()=>{
+        console.log("Closed transaction page")
+        modalContainer.style.display="none";
     })
 
 })
