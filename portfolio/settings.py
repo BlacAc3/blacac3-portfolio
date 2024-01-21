7dcae5e6dbdd2f13adf974def490d59b3b4fa4ca
BlacAc3 <aceezeala@gmail.com>
2024-01-21 13:40:26 +0100

    STatic issues

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 #creating and setting up our environment variables
+import os
 import environ
 env=environ.Env()
 environ.Env.read_env()
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
