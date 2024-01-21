8955929ef8a852c2896336d84e8352cf4ab2a3ce
BlacAc3 <aceezeala@gmail.com>
2024-01-21 14:06:17 +0100

    STatic issues more changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,15 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'admin'),
-    os.path.join(BASE_DIR, 'static', 'books'),
-    os.path.join(BASE_DIR, 'static', 'finance'),
-    os.path.join(BASE_DIR, 'static', 'mainpage'),
-    os.path.join(BASE_DIR, 'static', 'mycli'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
