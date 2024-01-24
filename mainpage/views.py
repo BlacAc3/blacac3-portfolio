b540294a416c6b0ce116995efc62773501b289ee
BlacAc3 <aceezeala@gmail.com>
2024-01-25 00:00:37 +0100

    Mini Changes

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import request
+from django.views.decorators.cache import cache_page
 # Create your views here.
+
+@cache_page(60*15)
 def index(request):
     return render(request, "main/main_index.html")
