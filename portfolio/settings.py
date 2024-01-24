dcaa4ce268e4fafba17aa1f839d84386923cc380
BlacAc3 <aceezeala@gmail.com>
2024-01-24 15:48:35 +0100

    Preparing for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
@@ -152,9 +152,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 
 
+#postgres database!!
+DATABASES = {
 
-# DATABASES = {
-
-#     "default":dj_database_url.parse(env("DATABASE_URL"))
+    "default":dj_database_url.parse(env("DATABASE_URL"))
     
-# }
+}
